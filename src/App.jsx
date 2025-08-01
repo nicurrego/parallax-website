77d772138fe9154c6ed173b6e89162e7b16ee3d6
Nicolas Urrego <nicurrego@gmail.com>
2025-08-01 21:40:25 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
+import About from './components/About.jsx';
+import ProjectsList from './components/ProjectsList.jsx';
+import ProjectArticle from './components/ProjectArticle.jsx';
+import Contact from './components/Contact.jsx';
+import ParallaxLayers from './components/ParallaxLayers.jsx';
+
+function App() {
+  return (
+    <Router>
+      <ParallaxLayers />
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<ProjectsList />} />
+          <Route path="/projects/:slug" element={<ProjectArticle />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* fallback to home */}
+          <Route path="*" element={<Hero />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
